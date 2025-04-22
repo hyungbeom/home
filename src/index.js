f34a8eb0a6f0a603c5ff8db25f82ac3652e39660
12gina <kjh@progist.co.kr>
2025-04-22 10:21:58 +0900

    tt

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './styles.css'
 import App from './App'
-import { Logo } from '@pmndrs/branding'
+import {Logo} from '@pmndrs/branding'
 
 function Overlay() {
     return (
         <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} className="fade-in">
-            <div style={{position: 'absolute', bottom: 0,fontSize: '23px', backgroundColor : 'white', color : 'black', opacity : 0.5}}>
+            <div style={{
+                position: 'absolute',
+                bottom: 0,
+                fontSize: '23px',
+                backgroundColor: 'white',
+                color: 'black',
+                opacity: 0.5
+            }}>
                 <marquee scrollamount="10">
-
                     &nbsp; &nbsp;&nbsp;&nbsp;                   &nbsp; &nbsp;&nbsp;&nbsp;
                     <img
                         src="/with.png" height={28} alt=""/>
@@ -38,9 +44,8 @@ function Overlay() {
 
             </div>
             <div style={{height: 60}}>
-                <img src="/logo.png" alt="" style={{padding: 20, cursor: 'point'}}/>
+                <img src="/logo.png" alt="" style={{padding: 20, cursor: 'pointer'}}/>
             </div>
-
 
 
         </div>
